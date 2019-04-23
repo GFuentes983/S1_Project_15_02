d4abe6e44a4d3a162ebff7d5d7e8f6b70402cb9b
Gabriel Fuentes <gfuentes983@west-mec.org>
2019-04-23 13:18:30 -0700

    Final Commit Ready for Review.
    Debugged issues that arose and managed to get all the code to function. The issue was a comment that wasn't supposed to be commented out and fixed some missing brackets.

--- dl_expense.js
+++ dl_expense.js
@@ -36,17 +36,16 @@ window.addEventListener('load', function() {
    var changingCells = document.querySelectorAll('table#travelExp input.sum');
    // Set all in the changingCells collection to run the calcExp function when changed
    for (var i = 0; i < changingCells.length; i++) {
-      //changingCells[i].onchange = calcExp();
+      changingCells[i].addEventListener('change', calcExp); }
    // Run the validateSummary function when the element with the id submitButton is changed
-   document.getElementById('submitButton').onchange = validateSummary();
-   }
-   calcClass();
+   document.getElementById('submitButton').onclick = function() {
+      validateSummary(); }
 });
 
 // Creating the validateSummary function
 function validateSummary() {
    // Creates a variable that will go for the text area with the id summary
-   var summary = document.querySelector('textarea#summary');
+   var summary = document.getElementById('summary');
    // Display a message if the field value is missing otherwise set to an empty text string
    if (summary.validity.valueMissing) {
       summary.setCustomValidity("You must include a summary of the trip in your report");
@@ -58,19 +57,37 @@ function validateSummary() {
 // Creating the calcClass function and passing it a parameter of sumClass
 function calcClass(sumClass) {
    // Variable of sumFields containing an object collection of elements to the sumClass class
-   var sumFields = document.querySelectorAll('.sumClass');
+   var sumFields = document.getElementsByClassName(sumClass);
    // Variable to keep a running total of the total values in the sumFields object collection
    var sumTotal = 0;
 
    for (var i = 0; i < sumFields.length; i++) {
-      var itemValue = parseFloat(sumFields[i]);
-      if (itemValue.NaN() === false) {
-        sumTotal = sumTotal + itemValue;
+      var itemValue = parseFloat(sumFields[i].value);
+      if (isNaN(itemValue) === false) {
+        sumTotal += itemValue;
       }
    }
    return sumTotal;
 }
 
+// calculate the row and column totals from the travelExp table
+function calcExp() {
+   // expTable referencing all the tr elements within the table body of the travelExp table
+   var expTable = document.querySelectorAll('table#travelExp tbody tr');
+
+   // For each row in the expTable object collection set the value of input value with the id subtotalIndex from the returned value of the calcClass function with the parameter dateIndex
+   for (var i = 0; i < expTable.length; i++) {
+      document.getElementById(`subtotal${i}`).value = formatNumber(calcClass(`date${i}`),2);
+   }
+
+   // Setting the values for cost for the trans,lodge, meal and other total variables.
+   document.getElementById('transTotal').value = formatNumber(calcClass("trans"),2 );
+   document.getElementById('lodgeTotal').value = formatNumber(calcClass("lodge"),2 );
+   document.getElementById('mealTotal').value = formatNumber(calcClass("meal"),2 );
+   document.getElementById('otherTotal').value = formatNumber(calcClass("other"),2 );
+
+   document.getElementById('expTotal') = formatUSCurrency(calcClass('sum'));
+}
 
 
 // Division -----------------------------------------------------
